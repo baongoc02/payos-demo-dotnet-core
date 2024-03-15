73a90625ac8c44ce9a5341b8edccd0fb72009f3b
baongoc02 <tranthibaongoc779152@gmail.com>
2024-03-15 21:22:09 +0700

    update

--- Controllers/CheckoutController.cs
+++ Controllers/CheckoutController.cs
@@ -37,10 +37,10 @@ public class CheckoutController : Controller
         try
         {
             int orderCode = int.Parse(DateTimeOffset.Now.ToString("ffffff"));
-            ItemData item = new ItemData("Mì tôm hảo hảo ly", 1, 1000);
+            ItemData item = new ItemData("Mì tôm hảo hảo ly", 1, 2000);
             List<ItemData> items = new List<ItemData>();
             items.Add(item);
-            PaymentData paymentData = new PaymentData(orderCode, 1000, "Thanh toan don hang", items, "https://localhost:3002/cancel", "https://localhost:3002/success");
+            PaymentData paymentData = new PaymentData(orderCode, 2000, "Thanh toan don hang", items, "https://localhost:3002/cancel", "https://localhost:3002/success");
 
             CreatePaymentResult createPayment = await _payOS.createPaymentLink(paymentData);
 
